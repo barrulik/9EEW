c22c91a72b77182c0280c2faf1c262eedd9fd2a1
barrulik <62114976+barrulik@users.noreply.github.com>
2020-12-30 13:15:58 +0200

    adding welcomer & antiraid

--- index.js
+++ index.js
@@ -31,14 +31,11 @@ client.on("error", console.error);
 
 const commandFiles = readdirSync("./commands").filter((file) => file.endsWith(".js"));
 for (const file of commandFiles) {
-  const command = require("./commands" + file);
+  const command = require("./commands/" + file);
   client.commands.set(command.name, command);
 }
 
 client.on("message", async (msg) => {
-  if (Date.now() - member.user.createdAt < 1000*60*60*24*10) {
-    msg.member.addRole(role);
-  }
   if (msg.author.bot) return;
 
   const arg = msg.content.substring(config.PREFIX.length).split(" ");
@@ -60,8 +57,22 @@ client.on("message", async (msg) => {
 });
 
 
-client.on("guildMemberAdd", member => {
-  if (Date.now() - member.user.createdAt < 1000*60*60*24*10) {
-    member.addRole(role);
+client.on('guildMemberAdd', async member => {
+  if (Date.now() - member.user.createdAt < 1000*60*60*24*config.new_account_days && config.AntiRaid) {
+    member.kick()
+    return;
+  }
+  if (config.Welcomer){ 
+    let username = member.user.username + "#" + member.user.discriminator;
+    let Discord = require('discord.js');
+    const Embed = new Discord.MessageEmbed()
+    .setColor('#ff0000')
+    .setTitle('Welcome')
+    .setThumbnail(member.user.avatarURL())
+    .setDescription(username + " just joined the server")
+    .setTimestamp(); 
+    setTimeout(() => {
+      client.channels.cache.get(config.welcomeChannel).send(Embed);
+    }, 1000);
   }
 });
